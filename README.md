a369e205b87bc1b2a6db4d3b8792d81bee398576
Tim Hellhake <tim-hellhake@users.noreply.github.com>
2019-05-28 23:50:55 +0200

    Release initial version 0.1.0

---
+++ README.md
@@ -0,0 +1,18 @@
+# Timer Adapter
+
+[![Build Status](https://travis-ci.org/tim-hellhake/timer-adapter.svg?branch=master)](https://travis-ci.org/tim-hellhake/timer-adapter)
+[![dependencies](https://david-dm.org/tim-hellhake/timer-adapter.svg)](https://david-dm.org/tim-hellhake/timer-adapter)
+[![devDependencies](https://david-dm.org/tim-hellhake/timer-adapter/dev-status.svg)](https://david-dm.org/tim-hellhake/timer-adapter?type=dev)
+[![optionalDependencies](https://david-dm.org/tim-hellhake/timer-adapter/optional-status.svg)](https://david-dm.org/tim-hellhake/timer-adapter?type=optional)
+[![license](https://img.shields.io/badge/license-MPL--2.0-blue.svg)](LICENSE)
+
+Runs for the specified number of seconds.
+
+## Configuration
+1. Go to settings
+2. Add a timer
+
+## Usage
+The timer has two actions:
+* `start`: Start the timer and set the `elapsed` property after the specified number of seconds
+* `reset`: Reset the timer to the specified number of seconds and clear the `elapsed` property
---
+++ timer-adapter.js
@@ -0,0 +1,97 @@
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.*
+ */
+
+'use strict';
+
+const {
+  Adapter,
+  Device,
+  Property
+} = require('gateway-addon');
+
+class Timer extends Device {
+  constructor(adapter, manifest, timer) {
+    super(adapter, timer.name);
+    this['@context'] = 'https://iot.mozilla.org/schemas/';
+    this.name = timer.name;
+    this.description = manifest.description;
+    this.callbacks = {};
+
+    const finishedProperty = this.createProperty({
+      type: 'boolean',
+      title: 'elapsed',
+      description: 'Whether the timer has elapsed',
+      readOnly: true
+    });
+
+    this.addCallbackAction('start', 'Start the timer', () => {
+      if (!this.timerHandle) {
+        console.log(`Starting timer ${timer.name}`);
+        this.timerHandle = setTimeout(() => {
+          finishedProperty.setCachedValue(true);
+          this.notifyPropertyChanged(finishedProperty);
+        }, timer.seconds * 1000);
+      }
+    });
+
+    this.addCallbackAction('reset', 'Reset the timer', () => {
+      if (this.timerHandle) {
+        console.log(`Resetting timer ${timer.name}`);
+        clearTimeout(this.timerHandle);
+        this.timerHandle = null;
+      }
+
+      finishedProperty.setCachedValue(false);
+      this.notifyPropertyChanged(finishedProperty);
+    });
+  }
+
+  createProperty(description) {
+    const property = new Property(this, description.title, description);
+    this.properties.set(description.title, property);
+    return property;
+  }
+
+  addCallbackAction(title, description, callback) {
+    this.addAction(title, {
+      title,
+      description
+    });
+
+    this.callbacks[title] = callback;
+  }
+
+  async performAction(action) {
+    action.start();
+
+    const callback = this.callbacks[action.name];
+
+    if (callback) {
+      callback();
+    } else {
+      console.warn(`Unknown action ${action.name}`);
+    }
+
+    action.finish();
+  }
+}
+
+class TimerAdapter extends Adapter {
+  constructor(addonManager, manifest) {
+    super(addonManager, TimerAdapter.name, manifest.name);
+    addonManager.addAdapter(this);
+    const timers = manifest.moziot.config.timers;
+
+    if (timers) {
+      for (const timer of timers) {
+        const device = new Timer(this, manifest, timer);
+        this.handleDeviceAdded(device);
+      }
+    }
+  }
+}
+
+module.exports = TimerAdapter;
