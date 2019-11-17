bee801c37b9f5bb928a2df418ab9a36d4bb6d33c
Tim Hellhake <tim-hellhake@users.noreply.github.com>
2019-11-17 01:32:45 +0100

    Add seconds property
    #2

--- src/timer-adapter.ts
+++ src/timer-adapter.ts
@@ -26,56 +26,47 @@ interface IntervalConfig {
 class Timer extends Device {
   private callbacks: { [name: string]: () => void } = {};
   private timerHandle?: NodeJS.Timeout;
+  private seconds: number = 0;
+  private finishedProperty: Property;
+  private runningProperty: Property;
+  private secondsProperty: Property;
 
-  constructor(adapter: Adapter, timer: TimerConfig) {
+  constructor(adapter: Adapter, private timer: TimerConfig) {
     super(adapter, `timer-${timer.id}`);
     this['@context'] = 'https://iot.mozilla.org/schemas/';
     this.name = timer.name;
     this.description = manifest.description;
-    this.callbacks = {};
 
-    const finishedProperty = this.createProperty({
+    this.finishedProperty = this.createProperty({
       type: 'boolean',
       title: 'elapsed',
       description: 'Whether the timer has elapsed',
       readOnly: true
     });
 
-    const runningProperty = this.createProperty({
+    this.runningProperty = this.createProperty({
       type: 'boolean',
       title: 'running',
       description: 'Whether the timer is currently running',
       readOnly: true
     });
 
+    this.secondsProperty = this.createProperty({
+      '@type': 'LevelProperty',
+      type: 'integer',
+      minimum: 0,
+      maximum: timer.seconds,
+      title: 'seconds',
+      description: 'The number of seconds',
+      readOnly: true
+    });
+
     this.addCallbackAction('start', 'Start the timer', () => {
-      if (!this.timerHandle) {
-        console.log(`Starting timer ${timer.name}`);
-        runningProperty.setCachedValue(true);
-        this.notifyPropertyChanged(runningProperty);
-
-        this.timerHandle = setTimeout(() => {
-          finishedProperty.setCachedValue(true);
-          this.notifyPropertyChanged(finishedProperty);
-
-          runningProperty.setCachedValue(false);
-          this.notifyPropertyChanged(runningProperty);
-        }, timer.seconds * 1000);
-      }
+      this.start();
     });
 
     this.addCallbackAction('reset', 'Reset the timer', () => {
-      if (this.timerHandle) {
-        console.log(`Resetting timer ${timer.name}`);
-        clearTimeout(this.timerHandle);
-        this.timerHandle = undefined;
-
-        runningProperty.setCachedValue(false);
-        this.notifyPropertyChanged(runningProperty);
-      }
-
-      finishedProperty.setCachedValue(false);
-      this.notifyPropertyChanged(finishedProperty);
+      this.reset();
     });
   }
 
@@ -107,14 +98,84 @@ class Timer extends Device {
 
     action.finish();
   }
+
+  private setFinished(value: boolean) {
+    this.finishedProperty.setCachedValue(value);
+    this.notifyPropertyChanged(this.finishedProperty);
+  }
+
+  private setRunning(value: boolean) {
+    this.runningProperty.setCachedValue(value);
+    this.notifyPropertyChanged(this.runningProperty);
+  }
+
+  private setSeconds(value: number) {
+    this.seconds = value;
+    this.secondsProperty.setCachedValue(value);
+    this.notifyPropertyChanged(this.secondsProperty);
+  }
+
+  private start() {
+    if (!this.timerHandle) {
+      console.log(`Starting timer ${this.timer.name}`);
+      this.setRunning(true);
+
+      this.timerHandle = setInterval(() => {
+        this.tick();
+      }, 1000);
+    }
+  }
+
+  private reset() {
+    if (this.timerHandle) {
+      console.log(`Resetting timer ${this.timer.name}`);
+      clearTimeout(this.timerHandle);
+      this.timerHandle = undefined;
+    }
+
+    this.setRunning(false);
+    this.setFinished(false);
+    this.setSeconds(0);
+  }
+
+  private finish() {
+    if (this.timerHandle) {
+      clearTimeout(this.timerHandle);
+      this.timerHandle = undefined;
+    }
+
+    this.setRunning(false);
+    this.setFinished(true);
+  }
+
+  private tick() {
+    this.setSeconds(this.seconds + 1);
+
+    if (this.seconds == this.timer.seconds) {
+      this.finish();
+    }
+  }
 }
 
 class Interval extends Device {
-  constructor(adapter: Adapter, interval: IntervalConfig) {
+  private seconds: number = 1;
+  private secondsProperty: Property;
+
+  constructor(adapter: Adapter, private interval: IntervalConfig) {
     super(adapter, `interval-${interval.id}`);
     this['@context'] = 'https://iot.mozilla.org/schemas/';
     this.name = interval.name;
     this.description = manifest.description;
+
+    this.secondsProperty = this.createProperty({
+      '@type': 'LevelProperty',
+      type: 'integer',
+      minimum: 0,
+      maximum: interval.seconds,
+      title: 'seconds',
+      description: 'The number of seconds',
+      readOnly: true
+    });
 
     this.events.set('elapsed', {
       name: 'elapsed',
@@ -125,14 +186,33 @@ class Interval extends Device {
     });
 
     setInterval(() => {
-      this.eventNotify(new Event(this, 'elapsed'));
-    }, interval.seconds * 1000);
+      this.tick();
+    }, 1000);
   }
 
   createProperty(description: any) {
     const property = new Property(this, description.title, description);
     this.properties.set(description.title, property);
     return property;
+  }
+
+  private setSeconds(value: number) {
+    this.secondsProperty.setCachedValue(value);
+    this.notifyPropertyChanged(this.secondsProperty);
+  }
+
+  private tick() {
+    this.seconds++;
+
+    if (this.seconds == this.interval.seconds) {
+      this.eventNotify(new Event(this, 'elapsed'));
+    }
+
+    if (this.seconds > this.interval.seconds) {
+      this.seconds = 1;
+    }
+
+    this.setSeconds(this.seconds);
   }
 }
 
