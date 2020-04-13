36b2f15ecbb03c91138d2a34a90afd4c4506e2d2
Tim Hellhake <tim-hellhake@users.noreply.github.com>
2020-04-13 16:04:59 +0200

    Add precision timers (#8)

--- src/timer-adapter.ts
+++ src/timer-adapter.ts
@@ -167,6 +167,65 @@ class Timer extends Device {
   }
 }
 
+class PrecisionTimer extends Device {
+  private callbacks: { [name: string]: () => void } = {};
+  private timerHandle?: NodeJS.Timeout;
+
+  constructor(adapter: Adapter, private timer: TimerConfig) {
+    super(adapter, `timer-${timer.id}`);
+    this['@context'] = 'https://iot.mozilla.org/schemas/';
+    this['@type'] = [];
+    this.name = timer.name;
+    this.description = manifest.description;
+
+    this.addCallbackAction('start', 'Start the timer', () => {
+      this.start();
+    });
+  }
+
+  addCallbackAction(title: string, description: string, callback: () => void) {
+    this.addAction(title, {
+      title,
+      description
+    });
+
+    this.events.set('elapsed', {
+      name: 'elapsed',
+      metadata: {
+        description: 'Timer elapsed',
+        type: 'string'
+      }
+    });
+
+    this.callbacks[title] = callback;
+  }
+
+  async performAction(action: any) {
+    action.start();
+
+    const callback = this.callbacks[action.name];
+
+    if (callback) {
+      callback();
+    } else {
+      console.warn(`Unknown action ${action.name}`);
+    }
+
+    action.finish();
+  }
+
+  private start() {
+    if (!this.timerHandle) {
+      debug(`Starting timer ${this.timer.name}`);
+
+      this.timerHandle = setTimeout(() => {
+        this.eventNotify(new Event(this, 'elapsed'));
+        this.timerHandle = undefined;
+      }, this.timer.seconds * 1000);
+    }
+  }
+}
+
 class Interval extends Device {
   private seconds: number = 1;
   private secondsProperty: Property;
@@ -228,6 +287,7 @@ class Interval extends Device {
 
 export class TimerAdapter extends Adapter {
   private timers: { [key: string]: Timer } = {};
+  private precisionTimers: { [key: string]: PrecisionTimer } = {};
   private intervals: { [key: string]: Interval } = {};
 
   constructor(addonManager: any, manifest: any) {
@@ -270,6 +330,16 @@ export class TimerAdapter extends Adapter {
       }
     }
 
+    if (config.precisionTimers) {
+      for (const precisionTimer of config.precisionTimers) {
+        if (!precisionTimer.id) {
+          precisionTimer.id = `${crypto.randomBytes(16).toString('hex')}`;
+        }
+
+        this.precisionTimers[precisionTimer.id] = new PrecisionTimer(this, precisionTimer);
+      }
+    }
+
     if (config.intervals) {
       for (const interval of config.intervals) {
         if (!interval.id) {
@@ -287,6 +357,11 @@ export class TimerAdapter extends Adapter {
     for (let id in this.timers) {
       const timer = this.timers[id];
       this.handleDeviceAdded(timer);
+    }
+
+    for (let id in this.precisionTimers) {
+      const precisionTimer = this.precisionTimers[id];
+      this.handleDeviceAdded(precisionTimer);
     }
 
     for (let id in this.intervals) {
